933ee3591a3c6d7c16acb8ebe7282681dc617573
JulioCASilva <jc.arruda9@gmail.com>
2025-01-15 17:49:26 -0300

    ajuste no codigo

--- portifolio/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from home import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('projetos/', views.projetos, name='projetos'),
-    path('sobre/', views.sobre, name='sobre'),
-]
